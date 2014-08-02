a6c89c944d72f8f3f3ea4e297d7f2037f7126d69
yhjhoo <yhjhoo@gmail.com>
2014-08-02 10:32:52 +0800

    1. correct index

--- me.princeSSHTest/src/test/java/testPrint.java
+++ me.princeSSHTest/src/test/java/testPrint.java
@@ -8,7 +8,7 @@ public class testPrint {
 		System.out.println("1");
 		System.out.println("2");
 		System.out.println("3");
-		System.out.println("4");
+		System.out.println("3");
 		System.out.println("5");
 	}
 }
