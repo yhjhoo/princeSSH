c3fb16a055cd73e54525041dd41207ac87f5f3b5
Wang Zi <Wang Zi@prince-Latitude-E6430>
2014-08-02 10:34:57 +0800

    1. commit junk

--- me.princeSSHTest/src/test/java/testPrint.java
+++ me.princeSSHTest/src/test/java/testPrint.java
@@ -10,5 +10,9 @@ public class testPrint {
 		System.out.println("3");
 		System.out.println("4");
 		System.out.println("5");
+		
+		System.out.println("6");
+		System.out.println("7");
+		System.out.println("9");
 	}
 }
